300a55f2f18eaa18ade98af2460fa32485308cee
Tobibur <tobiburrahman786@gmail.com>
2018-05-23 11:20:52 +0530

    Updated demo.pyquit

--- Data-Science-py/demo.py
+++ Data-Science-py/demo.py
@@ -3,8 +3,17 @@ from sklearn import tree
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.neural_network import MLPClassifier
+import numpy as np
 
-clf = [tree.DecisionTreeClassifier(),KNeighborsClassifier(n_neighbors=3),GaussianNB(),MLPClassifier(alpha=1)]
+clf = {
+	'DesicisionTree':tree.DecisionTreeClassifier(),
+	'KNeighborsClassifier':KNeighborsClassifier(n_neighbors=3),
+	'GaussianNB':GaussianNB(),
+	'MLPClassifier':MLPClassifier(alpha=1)
+	}
+
+#accuracy
+from sklearn.metrics import accuracy_score
 
 # [height, weight, shoe_size]
 X = [[181, 80, 44], [177, 70, 43], [160, 60, 38], [154, 54, 37], [166, 65, 40],
@@ -14,15 +23,22 @@ X = [[181, 80, 44], [177, 70, 43], [160, 60, 38], [154, 54, 37], [166, 65, 40],
 Y = ['male', 'male', 'female', 'female', 'male', 'male', 'female', 'female',
      'female', 'male', 'male']
 
-x_test = [[190, 70, 43]]
-
+i=0
+acc_tree = []
 # CHALLENGE - ...and train them on our data
 #print(prediction)
 for items in clf:
-	clfs = items.fit(X, Y)
-	prediction = items.predict(x_test)
-	print (items)
+	clfs = clf[items].fit(X, Y)
+	prediction = clf[items].predict(X)
+	acc_tree.append(accuracy_score(Y, prediction) * 100)
+	print(items)
+	print('Accuracy : {}'.format(acc_tree[i]))
 	print(prediction)
+	print()
+	i+=1
 
 
 # CHALLENGE compare their reusults and print the best one!
+index = np.argmax([acc_tree[0],acc_tree[1],acc_tree[2],acc_tree[3]])
+classifiers = {0: 'DesicisionTree', 1: 'KNeighborsClassifier', 2: 'GaussianNB', 3: 'MLPClassifier'}
+print('Best gender classifier is {}'.format(classifiers[index]))
